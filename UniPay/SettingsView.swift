0816ad45ae1cc4be64a443a767fd6a41023cbf8f
lillytoma <lilyanntoma@icloud.com>
2025-01-18 17:43:03 -0500

    commited file

---
+++ UniPay/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  UniPay
+//
+//  Created by Lilly Toma on 1/18/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
