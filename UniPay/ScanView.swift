5942f91996f9d1a71b678ce5986628c8f28a550f
lillytoma <lilyanntoma@icloud.com>
2025-01-18 17:57:21 -0500

    changes

---
+++ UniPay/ScanView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanView.swift
+//  UniPay
+//
+//  Created by Lilly Toma on 1/18/25.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanView()
+}
