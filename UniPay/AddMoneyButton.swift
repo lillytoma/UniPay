41813c6763c4d24f76a0369249e2b3c56a77d510
lillytoma <lilyanntoma@icloud.com>
2025-01-18 18:04:58 -0500

    added a new file and made a couple of changes in homeview/settingsview

---
+++ UniPay/AddMoneyButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddMoneyButton.swift
+//  UniPay
+//
+//  Created by Lilly Toma on 1/18/25.
+//
+
+import SwiftUI
+
+struct AddMoneyButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMoneyButton()
+}
