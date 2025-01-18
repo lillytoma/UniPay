c7d206bc6ca8c9ac50fb4cbab44595f984cabd82
Lukas Sagmani <lsagmani2003@gmail.com>
2025-01-18 17:46:30 -0500

    Change

--- UniPay/SettingsView.swift
+++ UniPay/SettingsView.swift
@@ -1,18 +1,59 @@
-//
-//  SettingsView.swift
-//  UniPay
-//
-//  Created by Lilly Toma on 1/18/25.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                List {
+                    NavigationLink(destination: AddBankAccountView()) {
+                        Text("Add Bank Account")
+                    }
+                    NavigationLink(destination: ChangeEmailView()) {
+                        Text("Change email")
+                    }
+                    NavigationLink(destination: ChangePasswordView()) {
+                        Text("Change Password")
+                    }
+                }
+                .listStyle(InsetGroupedListStyle())
+            }
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "gearshape")
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    SettingsView()
+struct AddBankAccountView: View {
+    var body: some View {
+        Text("Add Bank Account Page")
+            .navigationTitle("Add Bank Account")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ChangeEmailView: View {
+    var body: some View {
+        Text("Change Email Page")
+            .navigationTitle("Change email")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ChangePasswordView: View {
+    var body: some View {
+        Text("Change Password Page")
+            .navigationTitle("Change Password")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
 }
