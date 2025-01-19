1930ebd7b31c03bb15e596b847ed4e232e1ef5af
lillytoma <lilyanntoma@icloud.com>
2025-01-18 19:09:57 -0500

    commit

---
+++ UniPay/AddMoneyView.swift
@@ -0,0 +1,18 @@
+//
+//  AddMoneyView.swift
+//  UniPay
+//
+//  Created by Lilly Toma on 1/18/25.
+//
+
+import SwiftUI
+
+struct AddMoneyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMoneyView()
+}
