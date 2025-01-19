e00199ecb1e5638267b8ed85e06450ead3aecc03
lillytoma <lilyanntoma@icloud.com>
2025-01-19 10:53:52 -0500

    3 files

---
+++ UniPay/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  UniPay
+//
+//  Created by Lilly Toma on 1/19/25.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
